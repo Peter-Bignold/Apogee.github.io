229e319e40d591297b030994fa7445ebb4336487
Peter Lefaivre <noodlegamesdev@gmail.com>
2020-01-06 22:12:32 -0700

    Version 0.0.5

--- Apogee-HTML5-Shipping.data.js
+++ Apogee-HTML5-Shipping.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/Apogee/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 85, "filename": "/UE4CommandLine.txt"}, {"start": 85, "audio": 0, "end": 50217037, "filename": "/Apogee/Content/Paks/Apogee-HTML5.pak"}], "remote_package_size": 50217037, "package_uuid": "1bd52a23-907a-449b-a6b5-75985ad9b226"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 85, "filename": "/UE4CommandLine.txt"}, {"start": 85, "audio": 0, "end": 51276008, "filename": "/Apogee/Content/Paks/Apogee-HTML5.pak"}], "remote_package_size": 51276008, "package_uuid": "e49bfc43-1128-4d00-9ea6-4711703dbff4"});
 
 })();
